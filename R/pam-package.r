ec1c0a4689159dfbed22e4961d2b3bfc9659bd9d
adibender <bender.at.R@gmail.com>
2017-05-08 21:29:48 +0200

    Update package man page

--- R/pam-package.r
+++ R/pam-package.r
@@ -1,4 +1,17 @@
-#' pam.
+#' pam: Piece-wise exponential Additive Models
+#' 
+#' pam provides functions and utilities that facilitate fitting 
+#' Piece-wise Exponential (Additive) Models, including data transformation and 
+#' other convenience functions for pre- and postprocessing as well as plotting.
+#' 
+#' The best way to get an overview of the functionality provided and how to 
+#' fit PAMs is to view the vignettes included in this package. A summary 
+#' of the vignettes content is given below: 
+#' 
+#' \itemize{
+#' \item \code{basics}: Introduction to PAMs and basic modeling.
+#' \item \code{baseline}: Shows how to estimate and visualize baseline model 
+#' (without covariates) and comparison to respective Cox-PH model.} 
 #'
 #' @name pam
 #' @docType package
