52c8099a430a141102bac090177245e648e63f9e
fabian-s <fabian.scheipl@googlemail.com>
2017-05-16 15:14:52 +0200

    fixing PEMs

--- R/hazard.R
+++ R/hazard.R
@@ -62,21 +62,28 @@ get_hazard <- function(
 	...)  {
 
 	assert_data_frame(newdata, all.missing=FALSE)
-	assert_class(object, classes = c("gam", "glm", "lm"))
+	assert_class(object, classes = "glm")
 	type <- match.arg(type)
 
-	original_intervals <- unique(model.frame(object)$tend)
-	weird_timepoints <- !all(newdata$tend %in% original_intervals)
-	if (!is.null(newdata$tend) & weird_timepoints) {
-	  warning("'tend' in <newdata> may contain values not used in original fit.",
+	original_intervals <- if (inherits(object, "gam")) {
+	  unique(model.frame(object)$tend)
+	} else levels(model.frame(object)$interval)
+	prediction_intervals <- if (inherits(object, "gam")) {
+	  unique(newdata$tend)
+	} else levels(factor(newdata$interval))
+	weird_timepoints <- !all(prediction_intervals %in% original_intervals)
+	if (weird_timepoints) {
+	  warning("Intervals in <newdata> may contain values not used in original fit.",
 	    " Setting interval start or end points to values not used for",
 	    " original fit in <object> can invalidate the PEM assumption and yield",
       " incorrect predictions or fitted values. Proceed with caution!")
 	}
 
-	pred <- predict(object=object, newdata=newdata, se.fit=TRUE, type=type, ...) %>%
+	pred <-
+	  predict(object=object, newdata = newdata, se.fit = TRUE, type=type,
+	    ...)[c("fit", "se.fit")] %>%
 		bind_cols() %>%
-		rename(hazard=fit, se=se.fit) %>%
+		rename(hazard = fit, se = se.fit) %>%
 		mutate(
 			hazard = as.numeric(hazard),
 			se     = as.numeric(se))
