c48701d8a806d26e8fa4332d6709ea18a861f161
adibender <bender.at.R@gmail.com>
2018-05-02 22:38:25 +0200

    Add geom_surv, fix geom_stephazard. Add examples. Fixes #38
    Signed-off-by: adibender <bender.at.R@gmail.com>

--- R/geom_hazard.R
+++ R/geom_hazard.R
@@ -14,6 +14,32 @@
 #' @inheritParams ggplot2::geom_line
 #' @rdname geom_hazard
 #' @importFrom ggplot2 layer GeomLine
+#' @examples
+#' library(ggplot2)
+#' library(pammtools)
+#' ped <- tumor[10:50,] %>% as_ped(Surv(days, status)~1)
+#' pam <- pamm(ped_status ~ s(tend), data=ped)
+#' ndf <- make_newdata(ped, tend = unique(tend)) %>% add_hazard(pam)
+#' # piece-wise constant hazards
+#' ggplot(ndf, aes(x = tend, y = hazard)) +
+#'  geom_vline(xintercept = c(0, ndf$tend[c(1, (nrow(ndf)-2):nrow(ndf))]), lty = 3) +
+#'  geom_hline(yintercept = c(ndf$hazard[1:3], ndf$hazard[nrow(ndf)]), lty = 3) +
+#'  geom_stephazard() +
+#'  geom_step(col=2) +
+#'  geom_step(col=2, lty = 2, direction="vh")
+#'
+#' # comulative hazard
+#' ndf <- ndf %>% add_cumu_hazard(pam)
+#' ggplot(ndf, aes(x = tend, y = cumu_hazard)) +
+#'  geom_hazard() +
+#'  geom_line(col=2) # doesn't start at (0, 0)
+#'
+#' # survival probability
+#' ndf <- ndf %>% add_surv_prob(pam)
+#' ggplot(ndf, aes(x = tend, y = surv_prob)) +
+#'  geom_surv() +
+#'  geom_line(col=2) # doesn't start at c(0,1)
+#'
 #' @export
 geom_hazard <- function(
   mapping     = NULL,
@@ -24,17 +50,14 @@ geom_hazard <- function(
   show.legend = NA,
   inherit.aes = TRUE, ...) {
   layer(
-    data = data,
-    mapping = mapping,
-    stat = stat,
-    geom = GeomHazard,
-    position = position,
+    data        = data,
+    mapping     = mapping,
+    stat        = stat,
+    geom        = GeomHazard,
+    position    = position,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
-    params = list(
-      na.rm = na.rm,
-      ...
-    )
+    params      = list(na.rm = na.rm, ... )
   )
 }
 
@@ -59,9 +82,15 @@ GeomHazard <- ggproto(
 #' @rdname geom_hazard
 #' @importFrom ggplot2 layer GeomStep
 #' @export
-geom_stephazard <- function(mapping = NULL, data = NULL, stat = "identity",
-                      position = "identity", direction = "vh",
-                      na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...) {
+geom_stephazard <- function(
+  mapping     = NULL,
+  data        = NULL,
+  stat        = "identity",
+  position    = "identity",
+  direction   = "vh",
+  na.rm       = FALSE,
+  show.legend = NA,
+  inherit.aes = TRUE, ...) {
   layer(
     data        = data,
     mapping     = mapping,
@@ -92,7 +121,7 @@ GeomStepHazard <- ggproto(
   setup_data = function(data, params) {
     row1   <- data %>% group_by(group) %>% slice(1)
     row1$x <- 0
-    row1$y <- 0
+    row1$y <- row1$y
     data   <- bind_rows(row1, data)
     data[order(data$PANEL, data$group, data$x), ]
   }
