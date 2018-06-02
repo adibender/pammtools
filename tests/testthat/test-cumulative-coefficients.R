a11fee006b8b2f31747d852bcf3558c1caf6985f
adibender <bender.at.R@gmail.com>
2018-06-02 17:33:43 +0200

    Add tests for mgcv::bam fits. Fixes #49

--- tests/testthat/test-cumulative-coefficients.R
+++ tests/testthat/test-cumulative-coefficients.R
@@ -6,15 +6,16 @@ test_that("Cumulative coefficients work", {
   tumor<- tumor %>% dplyr::sample_n(100)
   tumor_ped <- tumor %>% as_ped(
     formula = Surv(days, status)~ age + complications)
-  pamm <- mgcv::gam(
-    formula = ped_status ~ s(tend, by = as.ordered(complications)) +
-      s(tend, by =age),
-    data   = tumor_ped,
-    family = poisson(),
-    offset = offset)
+  pam <- mgcv::gam(formula = ped_status ~ s(tend, by = as.ordered(complications)) +
+      s(tend, by =age), data   = tumor_ped, family = poisson(), offset = offset)
+  expect_warning(bam <- mgcv::bam(formula = ped_status ~ s(tend, by = as.ordered(complications)) +
+      s(tend, by =age), data   = tumor_ped, family = poisson(), offset = offset,
+      method = "REML"))
   # summary(pamm)
   # plot(pamm, page=1)
-  cumu_coef_pam <- get_cumu_coef(pamm, tumor_ped, terms=c("age", "complications"))
+  cumu_coef_bam <- get_cumu_coef(bam, tumor_ped, terms=c("age", "complications"))
+  expect_data_frame(cumu_coef_bam, nrows=100, ncols=6)
+  cumu_coef_pam <- get_cumu_coef(pam, tumor_ped, terms=c("age", "complications"))
   expect_data_frame(cumu_coef_pam, nrows=100, ncols=6)
   aalen <- timereg::aalen(survival::Surv(days, status)~ age + complications, data=tumor)
   cumu_coef_aalen <- get_cumu_coef(aalen, terms=c("age", "complications"))
