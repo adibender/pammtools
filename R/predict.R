893f700bbf62e089ea81baabebaa7198beed2d80
Andreas Bender <bender.at.R@gmail.com>
2020-01-02 14:46:38 +0100

    Rd fixes. Add pec to imports

--- R/predict.R
+++ R/predict.R
@@ -2,17 +2,18 @@
 #'
 #' @param object An object of class \code{pam_xgb}
 #' @param newdata A data set containing the same covariates as used for model
-#' fitting. If of class \code{data.frame}, the function will try to transform
-#' to the \code{xgb.DMatrix} format.
-#' @param times A vector of times for which predictions should be generated
+#' fitting. If of class \code{data.frame}, the function will transform the
+#' data to the PED format using the same settings as for the data used in
+#' estimation.
 #' @param type The type of prediction desired. Either hazard (\code{type = "hazard"}),
 #' cumulative hazard (\code{type = "cumu_hazard"}) or survival probability
 #' (\code{type = "surv_prob"}).
+#' @param ... Currently not used.
 #' @importFrom stats predict
-#' @importFrom purrr map_dfr
 #' @return A matrix of predictions containing one row per
 #' observation (row in newdata) and 1 column per  specified time in the
 #' \code{times} argument.
+#' @seealso pamm
 #' @export
 predict.pamm <- function(
   object,
@@ -48,8 +49,10 @@ predict.pamm <- function(
 
 #' S3 method for pamm objects for compatibility with package pec
 #'
+#' @inheritParams predict.pamm
 #' @importFrom pec predictSurvProb
 #' @importFrom purrr map
+#' @param times A vector of times for which predictions should be generated.
 #' @export
 predictSurvProb.pamm <- function(
   object,
@@ -79,6 +82,7 @@ predictSurvProb.pamm <- function(
 
   }
 
+  env_times <- times
   newdata[["pred"]] <- predict(
     unpam(object),
     newdata = newdata,
@@ -88,7 +92,7 @@ predictSurvProb.pamm <- function(
     group_by(.data$id) %>%
     mutate(pred = exp(-cumsum(.data$pred * .data$intlen))) %>%
     ungroup() %>%
-    filter(.data[["times"]] %in% .env[["times"]])
+    filter(.data[["times"]] %in% env_times)
 
   id <- unique(newdata[[id_var]])
   pred_list <- map(
