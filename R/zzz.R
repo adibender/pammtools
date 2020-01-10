658eadc490c6f782302038135479a90bcae75e38
Andreas Bender <bender.at.R@gmail.com>
2020-01-10 15:01:16 +0100

    Only new tidyr syntax is supported now.

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,11 @@
-tidyr_new_interface <- function() FALSE
+
 .onLoad <- function(libname=find.package("pammtools"), pkgname="pammtools") {
 
   if (getRversion() >= "2.5.1") {
 
     utils::globalVariables(".")
 
-    tidyr_new_interface <<- function() {
-      utils::packageVersion("tidyr") > "0.8.99"
-    }
-
   }
-
-
 
   invisible()
 
