7bae4ae41dee9f9b457e5dd7a16f173700e105f8
Philipp Kopper <philipp.kopper@am-ammersee.de>
2020-07-06 18:48:18 +0200

    add tests as_ped_cr

--- R/cr.R
+++ R/cr.R
@@ -1,181 +1,3 @@
-#' Wrapping function for PEMs with competing risks using stats::glm
-#' 
-#' This function serves as a wrapper for the glm function (stats) to be 
-#' applicable to competing risks. Competing risks are separately modelled 
-#' in their own glm. The results can be interpreted synoptically, 
-#' using the corresponding methods (summary etc.) or separately using the glm
-#' methods for each entry alone.
-#' The input is entered as for a single risk PEM.
-
-#' 
-#' @param formula an object of class formula or a string convertible to it.
-#' The model formula corresponding to a glm (from the stats package).
-#' @param family the family of the glm to be modelled. Only poisson is
-#'  reasonable. Hence, any other input will not be accepted.
-#' @param data A data.frame of class ped_cr that features time-to-event data
-#' and convariates. (see https://adibender.github.io/pammtools/)
-#' @param offset The offset for each observation. Contained in data.
-#' @param ... additional arguments passed to the glm function.
-#' @return a list of glms - one entry for a single competing risk.
-#' @import checkmate
-#' @importFrom stats glm
-#' @export
-#' @examples 
-#' library(pammtools)
-#' set.seed(31072019)
-#' df <- cbind.data.frame(
-#' x1 = runif (n, -3, 3),
-#' x2 = runif (n, 0, 6))
-#' # two component formula specifying cause specific hazards
-#' form <- ~ log(0.5) + x1 + x2 | log(0.9) + (-1) * x1 + (-1 * x2)
-#' df <- sim_pexp_cr(form, df, seq(0, 3, by = 0.25)) %>%
-#'  mutate(
-#'    cens_time = runif(n(), 0.5, 3),
-#'    status = if_else(cens_time < time, 0, 1),
-#'    time = pmin(time, cens_time),
-#'    type = status * type)
-#' df <- df[, - c(5, 6)]
-#' colnames(df)[7] <- "obs_times"
-#' ped_cr <- as_ped_cr(data = df, Surv(obs_times, status) ~ ., id = "id",
-#' cut = seq(0, max(df$obs_times), 0.25))
-#' pem_cr <- glm_cr(ped_status ~ interval + x1 + x2, 
-#'                  data = ped_cr, offset = offset, family = poisson())
-#' @author Philipp Kopper
-pem_cr <- function(formula, family = poisson, ped, offset, ...) {
-  #check_input(formula, ped, offset)
-  res <- vector(mode = "list", length = length(ped))
-  for (i in 1:length(res)) {
-    res[[i]] <- glm(formula = formula, family = family, 
-                    data = ped[[i]], offset = offset, ...)
-  }
-  names(res) <- attr(ped, "risks")
-  which_type <- c("sh", "cs")[c("sh", "cs") %in% class(ped)]
-  class(res) <- c("pem_cr", which_type)
-  attr(res, "risks") <- attr(ped, "risks")
-  #for methods
-  return(res)
-}
-
-#' Summary method for competing risk PEMs (piece-wise exp. models)
-#' 
-#' This function summarises the underlying models of a pem_cr object.
-#' The summaries are returned list-wise with each element belonging to
-#' one competing risk.
-#' @param pem_cr An object of class pem_cr where all elements are one 
-#' glm object. Each element should correspond to one partial competing risks
-#' model of a PEM.
-#' @return A list of summaries.
-#' @author Philipp Kopper
-summary.pem_cr <- function(pem_cr) {
-  summary_list <- vector(mode = "list", length = length(pem_cr))
-  names(summary_list) <- names(pem_cr)
-  for (i in 1:length(pem_cr)) {
-    pem_cr[[i]]$call <- ""
-    summary_list[[i]] <- summary(pem_cr[[i]])
-  }
-  names(summary_list) <- attr(pem_cr, "risks")
-  summary_list
-}
-
-#' Print method for competing risk PEMs (piece-wise exp. models)
-#' @param summary_list a list of summaries where each element is one summary
-#' for a glm. Each element should correspond to one partial competing risks
-#' model of a PEM.
-#' @return A (printed) list of summaries.
-#' @author Philipp Kopper
-print.pem_cr <- function(summary_list) {
-  for (i in 1:length(summary_list)) {
-    cat(paste("Risk:", names(summary_list)[i]))
-    print(summary_list[[i]])
-  }
-}
-
-#' Wrapping function for PAMs with competing risks using mgcv::gam
-#' 
-#' This function serves as a wrapper for the gam function (mgcv) to be 
-#' applicable to competing risks. Competing risks are separately modelled 
-#' in their own gam. The results can be interpreted synoptically, 
-#' using the corresponding methods (summary etc.) or separately using the glm
-#' methods for each entry alone.
-#' The input is entered as for a single risk PAM.
-#' 
-#' @param formula an object of class formula or a string convertible to it.
-#' The model formula corresponding to a gam (from the mgcv package).
-#' @param family the family of the gam to be modelled. Only poisson is
-#'  reasonable. Hence, any other input will not be accepted.
-#' @param data A data.frame of class ped_cr that features time-to-event data
-#' and convariates. (see https://adibender.github.io/pammtools/)
-#' @param offset The offset for each observation. Contained in data.
-#' @param ... additional arguments passed to the gam function.
-#' @return a list of gams - one entry for a single competing risk.
-#' @import checkmate
-#' @importFrom mgcv gam
-#' @export
-#' @author Philipp Kopper
-#' #' library(pammtools)
-#' set.seed(31072019)
-#' df <- cbind.data.frame(
-#' x1 = runif (n, -3, 3),
-#' x2 = runif (n, 0, 6))
-#' # two component formula specifying cause specific hazards
-#' form <- ~ log(0.5) + x1 + x2 | log(0.9) + (-1) * x1 + (-1 * x2)
-#' df <- sim_pexp_cr(form, df, seq(0, 3, by = 0.25)) %>%
-#'  mutate(
-#'    cens_time = runif(n(), 0.5, 3),
-#'    status = if_else(cens_time < time, 0, 1),
-#'    time = pmin(time, cens_time),
-#'    type = status * type)
-#' df <- df[, - c(5, 6)]
-#' colnames(df)[7] <- "obs_times"
-#' ped_cr <- as_ped_cr(data = df, Surv(obs_times, status) ~ ., id = "id",
-#' cut = seq(0, max(df$obs_times), 0.25))
-#' pem_cr <- gam_cr(ped_status ~ interval + x1 + x2, 
-#'                  data = ped_cr, offset = offset, family = poisson())
-pam_cr <- function(formula, family = gaussian(), 
-                   ped = list(), offset = NULL, bam = FALSE, ...) {
-  #check_input(formula, ped, offset)
-  res <- vector(mode = "list", length = length(ped))
-  for (i in 1:length(res)) {
-    if (bam) {
-      res[[i]] <- bam(formula = formula, family = family, 
-                      data = ped[[i]], offset = offset, ...)
-    } else {
-      res[[i]] <- gam(formula = formula, family = family, 
-                      data = ped[[i]], offset = offset, ...)
-    }
-  }
-  names(res) <- attr(ped, "risks")
-  which_type <- c("sh", "cs")[c("sh", "cs") %in% class(ped)]
-  class(res) <- c("pam_cr", "pem_cr", which_type)
-  attr(res, "risks") <- attr(ped, "risks")
-  #for methods
-  return(res)
-}
-
-#' Summary method for competing risk PAMs (piece-wise additive models)
-#' 
-#' This function summarises the underlying models of a pem_cr object.
-#' The summaries are returned list-wise with each element belonging to
-#' one competing risk.
-#' @param pem_cr An object of class pem_cr where all elements are one 
-#' glm object. Each element should correspond to one partial competing risks
-#' model of a PEM.
-#' @return A list of summaries.
-#' @author Philipp Kopper
-summary.pam_cr <- function(pam_cr) {
-  summary.pem_cr(pam_cr)
-}
-
-#' Print method for competing risk PAMs (piece-wise additive models)
-#' @param summary_list a list of summaries where each element is one summary
-#' for a gam. Each element should correspond to one partial competing risks
-#' model of a PAM.
-#' @return A (printed) list of summaries.
-#' @author Philipp Kopper
-print.pam_cr <- function(summary_list) {
-  print.pem_cr(summary_list)
-}
-
 #' Transform data to Piece-wise Exponential Data (PED) for a Cause-Specific
 #' hazards competing risks model
 #'
@@ -204,18 +26,20 @@ print.pam_cr <- function(summary_list) {
 #' @export
 #' @import pammtools
 #' @author Philipp Kopper
-as_ped_cr <- function(data, formula, id, censor_code = 0L, 
-                      output = c("list", "data.frame"), cut = NULL, ...) {
+as_ped_cr <- function(data, formula, censor_code = 0L, 
+                      output = c("data.frame", "list"), cut = NULL, ...) {
   if (!(is.data.frame(data))) {
     f_data <- data[[1L]]
     assert_data_frame(data[[1L]])
-    assert_data_frame(data[[2L]])
+    if (length(data) == 2L) {
+      assert_data_frame(data[[2L]])
+    }
   } else {
     f_data <- data
     assert_data_frame(f_data)
   }
   assert_formula(formula)
-  output <- match.arg(output, c("list", "data.frame"))
+  output <- match.arg(output, c("data.frame", "list"))
   time_str <- all.vars(formula)[1L]
   status_str <- all.vars(formula)[2L]
   true_time <- f_data[[time_str]]
@@ -232,11 +56,11 @@ as_ped_cr <- function(data, formula, id, censor_code = 0L,
     current_data <- f_data
     current_data[[status_str]][f_data[[status_str]] != status[i]] <- 0L
     current_data[[status_str]][f_data[[status_str]] == status[i]] <- 1L
-    if (!is.data.frame(data)) {
+    if (!is.data.frame(data) & length(data) == 2L) {
       current_data <- list(current_data, data[[2L]])
     }
     ped_sets[[i]] <- as_ped(data = current_data, formula = formula, 
-                            id = id, cut = cut[[i]], ...)
+                            cut = cut[[i]], ...)
     if (output != "list") {
       ped_sets[[i]]$cause <- i
     }
@@ -250,80 +74,12 @@ as_ped_cr <- function(data, formula, id, censor_code = 0L,
   } else {
     ped <- as.data.frame(Reduce("rbind", ped_sets))
     class(ped) <- c("ped_cr_df", "ped_cr", "data.frame")
+    attr(ped, "intvars") <- c(attr(ped, "intvars"), "cause")
+    attr(ped, "breaks") <- cut
+    attr(ped, "trafo_args")[["cut"]] <- cut
     attr(ped, "risks") <- status
     return(ped)
   }
-}
-
-#' Helper function which creates the CR data frame for one specific risk.
-#' 
-#' @param data a ped_cr data.frame. 
-#' @param cr The competing risk which we aim a data frame for. 
-#' Needs to have the same name / number as in data.
-#' @return a ped data.frame object for a single competing risk. 
-#' (All other risks are treated as censoring.)
-#' @author Philipp Kopper
-modify_cr_data <- function(data, cr) {
-  data$ped_status <- ifelse(data$ped_status == cr, 1, 0)
-  data
-}
-
-#' Fitting of cr PEMs / PAMMs
-#' 
-#' This function fits a gams/glms depending on the input for a cr framework.
-#' It is the operative sub-routine of pam_cr / pem_cr.
-#' @param formula an object of class formula or a string convertible to it.
-#' The model formula corresponding to a gam or glm.
-#' @param family the family of the gam to be modelled. Only poisson is
-#'  reasonable. Hence, any other input will not be accepted.
-#' @param data A data.frame of class ped_cr that features time-to-event data
-#' and convariates. (see https://adibender.github.io/pammtools/)
-#' @param offset The offset for each observation. Contained in data.
-#' @param m_type A character indicating the type of model to be fit: either 
-#' "glm" or "gam".
-#' @param ... additional arguments passed to the gam/glm function.
-#' @return a list of gams or glms - one entry for a single competing risk.
-#' @importFrom mgcv gam
-#' @importFrom stats glm
-#' @author Philipp Kopper
-fit_cr <- function(formula, family, data, offset, m_type, ...) {
-  crs <- attr(data, "risks")
-  n_crs <- length(crs)
-  res <- vector(mode = "list", length = n_crs)
-  names(res) <- crs
-  for (i in 1:n_crs) {
-    # this function is supposed to make a ped_cr object to a ped object
-    # where we only investiagte one of the competing risks
-    current_data <- modify_cr_data(data, cr = crs[i])
-    command <- paste(m_type, "(formula = formula, family = family, ", 
-                     "data = current_data, offset = offset, ...)", sep = "")
-    res[[i]] <- eval(parse(text = command)) # verpönt: invoke!
-  }
-  return(res)
-}
-
-#' Input checking for cr PEMs / PAMMs
-#' 
-#' This function fits a gams/glms depending on the input for a cr framework.
-#' It is the operative sub-routine of glm_cr and gam_cr.
-#' @param formula an object of class formula or a string convertible to it.
-#' The model formula corresponding to a gam or glm.
-#' @param data A data.frame of class ped_cr that features time-to-event data
-#' and convariates. (see https://adibender.github.io/pammtools/)
-#' @param offset The offset for each observation. Contained in data.
-#' @return An assertion if there is false input.
-#' @import checkmate
-#' @author Philipp Kopper
-check_input <- function(formula, data, offset) {
-  assert_formula(formula)
-  if (!("ped_cr_subdist" %in% class(data))) {
-    assert_data_frame(data)
-  } else {
-    for (i in 1:length(data)) {
-      assert_data_frame(data[[i]])
-    }
-  }
-  if (is.null(offset)) stop("You need to specifiy an offset.")
 }
 
 #' Conversion to numeric risks
@@ -353,7 +109,7 @@ make_numeric <- function(data, status_str, censor_code) {
 }
 
 check_cuts <- function(cut, status) {
-  if (is.null(status)) {
+  if (is.null(cut)) {
     return(rep(list(cut), length(status)))
   }
   if ((!is.list(cut)) & (!is.numeric(cut))) {
---
+++ tests/testthat/test-as-ped-cr.R
@@ -0,0 +1,94 @@
+context("Test as_ped_cr functions")
+
+test_that("Trafo works and attributes are appended for data.frame.", {
+  # preparations
+  data("sir.adm", package = "mvna")
+  sir_adm <- sir.adm[c(1, 2, 3, 26, 40, 43, 50), ]
+  ped <- as_ped_cr(
+    data    = sir_adm,
+    formula = Surv(time, status) ~ age + pneu,
+    cut     = c(0, 10, 100),
+    output  = "data.frame")
+  # retransform to ped
+  expect_data_frame(ped, nrow = 12L * 2L, ncols = 9L)
+  expect_is(ped, "ped_cr_df")
+  expect_subset(c("ped_status", "tstart", "tend", 
+                  "interval", "offset", "cause"), names(ped))
+  expect_is(attr(ped, "breaks"), "list")
+  expect_is(attr(ped, "breaks")[[1]], "numeric")
+  expect_is(attr(ped, "breaks")[[2]], "numeric")
+  expect_is(attr(ped, "intvars"), "character")
+  expect_is(attr(ped, "id_var"), "character")
+  expect_equal(attr(ped, "id_var"), "id")
+  expect_equal(sum(ped$cause), 36)
+  
+  ped <- as_ped_cr(
+    data = sir_adm,
+    formula = Surv(time, status) ~ .,
+    output = "data.frame")
+  expect_data_frame(ped, nrows = 33L, ncols = 10L)
+  expect_equal(sum(ped$cause), 44L)
+  
+})
+
+test_that("Trafo for list output is identical to data.frame output.", {
+  # preparations
+  data("sir.adm", package = "mvna")
+  sir_adm <- sir.adm[c(1, 2, 3, 26, 40, 43, 50), ]
+  ped_df <- as_ped_cr(
+    data    = sir_adm,
+    formula = Surv(time, status) ~ age + pneu,
+    cut     = c(0, 10, 100),
+    output  = "data.frame")
+    ped_list <- as_ped_cr(
+      data    = sir_adm,
+      formula = Surv(time, status) ~ age + pneu,
+      cut     = c(0, 10, 100),
+      output  = "list")
+    ped_df_1 <- ped_df[ped_df$cause == 1, 1:8]
+    ped_df_2 <- ped_df[ped_df$cause == 2, 1:8]
+    expect_equal(sum(ped_df_1[, c(1:3, 5:8)] - ped_list[[1]][, c(1:3, 5:8)]), 0)
+    expect_equal(sum(ped_df_2[, c(1:3, 5:8)] - ped_list[[2]][, c(1:3, 5:8)]), 0)
+    expect_equal(ped_df_1[, 4], ped_list[[1]][, 4])
+    expect_equal(ped_df_2[, 4], ped_list[[2]][, 4])
+})
+
+test_that("Trafo works for list objects (with TDCs)", {
+  data("patient")
+  event_df  <- filter(patient, CombinedID %in% c(1110, 1116, 1316))
+  event_df$PatientDied[3] <- 2L
+  ped <- as_ped_cr(data = list(event_df), formula = Surv(survhosp, PatientDied)~ .,
+                cut = 0:30, id = "CombinedID")
+  expect_data_frame(ped, nrows = 70 * 2, ncols = 16)
+  tdc_df    <- filter(daily, CombinedID  %in% c(1110, 1116, 1316))
+  ## check nesting
+  ped <- as_ped_cr(
+    data    = list(event_df, tdc_df),
+    formula = Surv(survhosp, PatientDied) ~ . +
+      cumulative(survhosp, Study_Day, caloriesPercentage, tz_var = "Study_Day") +
+      cumulative(proteinGproKG, tz_var = "Study_Day"),
+    cut     = 0:30,
+    id  = "CombinedID")
+  expect_subset("survhosp_Study_Day_mat", colnames(ped))
+  expect_data_frame(ped, nrows = 140, ncols = 21)
+  expect_identical(any(is.na(ped$caloriesPercentage_Study_Day)), FALSE)
+  expect_identical(colnames(ped$Study_Day), paste0("Study_Day", 1:12))
+  ped <- as_ped_cr(
+    data    = list(event_df, tdc_df),
+    formula = Surv(survhosp, PatientDied) ~ . +
+      cumulative(Study_Day, caloriesPercentage, tz_var = "Study_Day") +
+      cumulative(proteinGproKG, tz_var = "Study_Day"),
+    id  = "CombinedID")
+  expect_data_frame(ped, nrows = 3 * 2, ncols = 20)
+  
+})
+
+test_that("Single risk not accepted.", {
+  data("patient")
+  event_df  <- filter(patient, CombinedID %in% c(1110, 1116, 1316))
+  expect_error(
+    as_ped_cr(data = list(event_df), formula = Surv(survhosp, PatientDied)~ .,
+              cut = 0:30, id = "CombinedID"), 
+    "There are no competing risks. Use as_ped() instead.", fixed = TRUE)
+})
+
