4ad799801fa74afb8872cae9764a7562b5ba2638
Andreas Bender <bender.at.R@gmail.com>
2020-08-18 13:17:46 +0200

    as_ped now also works for competing risks

--- R/as-ped.R
+++ R/as-ped.R
@@ -53,20 +53,31 @@ as_ped.data.frame <- function(
   cut          = NULL,
   max_time     = NULL,
   tdc_specials = c("concurrent", "cumulative"),
+  censor_code = 0L,
   ...) {
 
   status_error(data, formula)
   assert_subset(tdc_specials, c("concurrent", "cumulative"))
 
-  dots <- list(...)
-  dots$data <- data
-  formula <- get_ped_form(formula, data = data, tdc_specials = tdc_specials)
-  dots$formula  <- formula
-  dots$cut <- cut
-  dots$max_time <- max_time
-  ped <- do.call(split_data, dots)
-  attr(ped, "time_var") <- get_lhs_vars(formula)[1]
-  attr(ped, "status_var") <- get_lhs_vars(formula)[2]
+  event_types <- get_event_types(data, formula, censor_code)
+  if (length(event_types) > 1) {
+
+    ped <- as_ped_cr(data = data, formula = formula, cut = cut, max_time = max_time,
+      tdc_specials = tdc_specials, censor_code = censor_code, ...)
+
+  } else {
+
+    dots          <- list(...)
+    dots$data     <- data
+    dots$formula  <- get_ped_form(formula, data = data, tdc_specials = tdc_specials)
+    dots$cut      <- cut
+    dots$max_time <- max_time
+
+    ped <- do.call(split_data, dots)
+    attr(ped, "time_var") <- get_lhs_vars(dots$formula)[1]
+    attr(ped, "status_var") <- get_lhs_vars(dots$formula)[2]
+
+  }
 
   ped
 
@@ -195,5 +206,80 @@ as_ped.pamm <- function(data, newdata, ...) {
   trafo_args      <- data[["trafo_args"]]
   trafo_args$data <- newdata
   do.call(split_data, trafo_args)
+
+}
+
+
+#' Competing risks trafo
+#' @inherit as_ped
+#' @importFrom rlang .env
+#'
+#' @keywords internal
+as_ped_cr <- function(
+  data,
+  formula,
+  cut          = NULL,
+  max_time     = NULL,
+  tdc_specials = c("concurrent", "cumulative"),
+  censor_code  = 0L,
+  combine      = TRUE,
+  ...) {
+
+  lhs_vars <- get_lhs_vars(formula)
+  event_types <- get_event_types(data, formula, censor_code)
+
+  cut <- map2(
+    event_types,
+    ifelse(is.list(cut), cut, list(cut)),
+    function(.event, .cut) {
+      get_cut(data, formula = formula, cut = .cut, max_time = NULL, event = .event)
+    }
+  )
+  if(length(cut) > 1 & combine) {
+    cut <- list(do.call(union, cut))
+  }
+
+  ped <- map2(
+    event_types,
+    cut,
+    function(.event, .cut) {
+      ped_i <- data %>%
+        mutate(!!lhs_vars[2] := 1L * (.data[[lhs_vars[2]]] == .env[[".event"]])) %>%
+        as_ped(
+          formula      = formula,
+          cut          = .cut,
+          max_time     = max_time,
+          tdc_specials = tdc_specials,
+          ...)
+      ped_i$cause <- .event
+      ped_i
+    })
+
+  if (combine) {
+    ped <- do.call(rbind, ped)
+    class(ped) <- c("ped_cr_union", "ped_cr", class(ped))
+    attr(ped, "intvars") <- c(attr(ped, "intvars"), "cause")
+    attr(ped, "breaks") <- cut
+  } else {
+    class(ped) <- c("ped_cr_list", "ped_cr", "ped", class(ped))
+    names(ped) <- event_types
+    attributes(ped)$trafo_args$id <- attributes(ped[[1]])$trafo_args$id
+    attributes(ped)$trafo_args$formula <- formula
+  }
+
+  attr(ped, "trafo_args")[["cut"]] <- cut
+  attr(ped, "trafo_args")[["combine"]] <- combine
+  attr(ped, "risks") <- event_types
+
+  ped
+
+}
+
+#' exctract event types
+get_event_types <- function(data, formula, censor_code) {
+
+  lhs_vars <- get_lhs_vars(formula)
+  status_values <- unique(data[[lhs_vars[2]]]) %>% sort()
+  status_values[status_values != censor_code]
 
 }
--- tests/testthat/test-as-ped-cr.R
+++ tests/testthat/test-as-ped-cr.R
@@ -1,25 +1,45 @@
 context("Test as_ped_cr functions")
 
-test_that("Trafo works and attributes are appended for joint PED object..", {
+test_that("Trafo works and attributes are appended.", {
   # preparations
-  ped <- as_ped_cr(
+  ped <- as_ped(
     data         = sir_adm,
     formula      = Surv(time, status) ~ age + pneu,
     cut          = c(0, 10, 100)
   )
-  # retransform to ped
   expect_data_frame(ped, nrow = 12L * 2L, ncols = 9L)
   expect_is(ped, "ped_cr_union")
   expect_subset(c("ped_status", "tstart", "tend",
                   "interval", "offset", "cause"), names(ped))
   expect_is(attr(ped, "breaks"), "list")
   expect_is(attr(ped, "breaks")[[1]], "numeric")
-  expect_is(attr(ped, "breaks")[[2]], "numeric")
   expect_is(attr(ped, "intvars"), "character")
   expect_is(attr(ped, "id_var"), "character")
   expect_equal(attr(ped, "id_var"), "id")
   expect_equal(sum(as.numeric(ped$cause)), 36)
 
+  # check that trafo can be recovered
+  ped2 <- as_ped(ped, newdata = sir_adm)
+  expect_equal(ped, ped2, check.attributes = FALSE)
+
+  # check that list output identical for given cut points
+  ped_list <- as_ped(
+    data    = sir_adm,
+    formula = Surv(time, status) ~ age + pneu,
+    cut     = c(0, 10, 100),
+    combine = FALSE)
+  ped2 <- do.call(rbind, ped_list)
+  expect_true(all.equal(do.call(rbind, ped_list), ped, check.attributes = FALSE))
+  expect_identical(length(ped_list), 2L)
+  expect_identical(class(ped_list), c("ped_cr_list", "ped_cr", "ped", "list"))
+  expect_identical(names(attributes(ped_list)), c("class", "names", "trafo_args", "risks"))
+  expect_identical(length(attr(ped_list, "trafo_args")$cut), 2L)
+
+  # check that trafo can be recovered for ped list objects
+  ped_list2 <- as_ped(ped_list, newdata = sir_adm)
+  expect_equal(ped_list, ped_list2, check.attributes = FALSE)
+
+  # test when split points not specified
   ped <- as_ped_cr(
     data         = sir_adm,
     formula      = Surv(time, status) ~ .
@@ -27,26 +47,13 @@ test_that("Trafo works and attributes are appended for joint PED object..", {
   expect_data_frame(ped, nrows = 56L, ncols = 10L)
   expect_equal(sum(as.numeric(ped$cause)), 84L)
 
-})
+  ped_list <- as_ped_cr(sir_adm, Surv(time, status) ~ ., combine = FALSE)
+  expect_identical(attr(ped_list[[1]], "breaks"), c(4L, 10L, 24L, 37L, 101L))
+  expect_identical(attr(ped_list[[2]], "breaks"), c(22L, 25L))
+  ped_list2 <- as_ped(ped_list, newdata = sir_adm)
+  expect_equal(ped_list, ped_list2, check.attributes = FALSE)
 
-test_that("Trafo for list output is identical to joint output.", {
-  # preparations
-  ped_union <- as_ped_cr(
-    data    = sir_adm,
-    formula = Surv(time, status) ~ age + pneu,
-    cut     = c(0, 10, 100),
-    combine = TRUE)
-  ped_list <- as_ped_cr(
-    data    = sir_adm,
-    formula = Surv(time, status) ~ age + pneu,
-    cut     = c(0, 10, 100),
-    combine = FALSE)
-    ped_union_1 <- ped_union[ped_union$cause == 1, 1:8]
-    ped_union_2 <- ped_union[ped_union$cause == 2, 1:8]
-    expect_equal(sum(ped_union_1[, c(1:3, 5:8)] - ped_list[[1]][, c(1:3, 5:8)]), 0)
-    expect_equal(sum(ped_union_2[, c(1:3, 5:8)] - ped_list[[2]][, c(1:3, 5:8)]), 0)
-    expect_equal(ped_union_1[, 4], ped_list[[1]][, 4])
-    expect_equal(ped_union_2[, 4], ped_list[[2]][, 4])
+
 })
 
 test_that("Default cut behaviour works.", {
