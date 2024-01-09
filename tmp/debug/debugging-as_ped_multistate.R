2034a1aefd380cfec15f39c2a629cfa6b20f9254
Piller <Johannes.Piller@lmu.de>
2024-01-09 19:39:13 +0100

    Added split-data.R functions to debug

--- tmp/debug/debugging-as_ped_multistate.R
+++ tmp/debug/debugging-as_ped_multistate.R
@@ -1,4 +1,4 @@
-as_ped_multistate <- function(
+debug_as_ped_multistate <- function(
     data,
     formula,
     cut          = NULL,
@@ -32,7 +32,7 @@ as_ped_multistate <- function(
   dots$min_events <- min_events
   dots$timescale  <- timescale
   
-  #ped <- do.call(split_data_multistate, dots)
+  # ped <- do.call(split_data_multistate, dots)
   #attr(ped, "time_var")   <- get_lhs_vars(dots$formula)[1]
   
   return(dots)
@@ -57,6 +57,7 @@ id         <- "id"
 timescale  <- "gap"
 min_events <- 1L
 censor_code<- 0L
+max_time <- 3
 
 # add transition column to formula if not already specified
 rhs_vars <- get_rhs_vars(formula)
@@ -65,7 +66,7 @@ if (!(transition %in% rhs_vars)) {
 }
 
 # get dots object
-dots <- as_ped_multistate(
+dots <- debug_as_ped_multistate(
   data       = test_df,
   formula    = Surv(tstart, tstop, status)~ enum + age,
   transition = "enum",
@@ -73,12 +74,133 @@ dots <- as_ped_multistate(
   timescale  = "gap",
   max_time   = 3)
 
+# # get ped object
+# ped <- debug_as_ped_multistate(
+#   data       = test_df,
+#   formula    = Surv(tstart, tstop, status)~ enum + age,
+#   transition = "enum",
+#   id         = "id",
+#   timescale  = "gap",
+#   max_time   = 3)
+
 # call split function
 ped <- do.call(split_data_multistate, dots)
 # error: splits not at correct time
 # -> debug split_data_multistate
 
-split_data_multistate <- function(
+# ----------------------------------------------------------------------------#
+# copied code from split-data.R
+# ----------------------------------------------------------------------------#
+
+# ----------------------------------------------------------------------------#
+# line 185 onwards
+# ----------------------------------------------------------------------------#
+## extract names for event time and status variables
+surv_vars <- all.vars(update(formula, .~0))
+vars <- if ("." %in% all.vars(formula)) {
+  names(data)
+} else {
+  all.vars(formula)
+}
+uvars <- union(surv_vars, vars)
+if (!all(uvars %in% vars)) {
+  stop(paste("Variables provided in formula not in data set:",
+             paste0(setdiff(uvars, vars), collapse = ", ")))
+}
+# works correctly
+
+# ----------------------------------------------------------------------------#
+# line 197 onwards
+# ----------------------------------------------------------------------------#
+## obtain interval breaks points for each spell
+#comment out if-clause as timescale == gap
+#if(timescale == "gap") {
+  data <- mutate(data, !!!list(.time = quo(!!as.name(surv_vars[2]) - !!as.name(surv_vars[1]))))
+  formula <- update(formula, Surv(.time, status)~.)
+  formula <- update_formula(formula, proposed_names = c(".time", surv_vars[3]))
+  # JP 09.01.2024: What does the second update_formula do? -> try  to run without it
+#}
+# split data for each spell
+data_list <- split(data, data[[transition]])
+# rm(data)
+# only keep spells with minimum number of events
+data_list <- data_list[map_dbl(data_list, ~sum(.x[[surv_vars[3]]])) >= min_events]
+cuts <-  do.call(debug_split_data_multistate, dots)
+# error message: no applicable method for 'get_cut' applied to an object of class "list"
+# try within the function
+
+lhs_vars <- get_lhs_vars(formula)
+if (length(lhs_vars) == 3 & timescale == "gap") {
+  rhs_vars <- get_rhs_vars(formula)
+  formula_cuts <- as.formula(
+    paste0("Surv(", lhs_vars[2], ",", lhs_vars[3], ") ~ ",
+           paste(rhs_vars, collapse = "+")))
+} else {
+  formula_cuts <- formula
+}
+
+dots <- debug_split_data_multistate(
+          formula = dots$formula,
+          data = dots$data,
+          transition = dots$transition,
+          cut = dots$cut,
+          max_time = dots$max_time,
+          event = dots$event,
+          min_events = dots$min_events,
+          timescale = dots$timescale
+)
+
+
+split_df_list <- map2(
+  .x = data_list,
+  .y = ifelse(is.list(cuts), cuts, list(cuts)),
+  .f = ~ {
+    dots$data    <- .x
+    dots$formula <- formula
+    dots$cut     <- .y
+    split_df     <- do.call(split_data, dots)
+  }
+)
+
+split_df <- bind_rows(split_df_list)
+split_df <- split_df %>%
+  arrange(.data[[transition]], .data[[dots$id]], .data[["tstart"]])
+
+# remove all obs beyond last observed event time
+if (is.null(max_time)) {
+  max_time <- max(split_df[["tend"]][split_df[["ped_status"]] == 1])
+  split_df <- split_df %>% filter(.data[["tend"]] <= max_time)
+}
+
+# if (timescale == "calendar") {
+#   split_check <- split_df %>%
+#     group_by(.data[[dots$id]]) %>%
+#     summarize(dups = sum(duplicated(.data[["tstart"]])))
+
+#   if (any(split_check[["dups"]]) != 0) {
+#     stop("Something went wrong during data transformation. \n Please post an issue at 'https://github.com/adibender/pammtools/issues' with your code and data")
+#   }
+# }
+
+## set class and and attributes
+class(split_df) <- c("ped", class(split_df))
+attr(split_df, "breaks") <- cuts
+attr(split_df, "id_var") <- dots_in$id <- id_var
+attr(split_df, "intvars") <- c(id_var, "tstart", "tend", "interval", "offset",
+                               "ped_status")
+dots_in$transition           <- transition
+dots_in$timescale            <- timescale
+dots_in$cut                  <- sort(unique(cuts))
+dots_in$max_time             <- max_time
+dots_in$event                <- event
+dots_in$min_events           <- min_events
+attr(split_df, "trafo_args") <- dots_in
+class(split_df)              <- unique(class(split_df))
+
+split_df
+
+
+debug_split_data_multistate <- function(
     formula,
     data,
     transition    = character(),
@@ -130,13 +252,14 @@ split_data_multistate <- function(
   # rm(data)
   # only keep spells with minimum number of events
   data_list <- data_list[map_dbl(data_list, ~sum(.x[[surv_vars[3]]])) >= min_events]
-  cuts <- get_cut(data_list, formula, cut = cut, max_time = max_time,
+  cuts <- get_cut.list(data_list, formula, cut = cut, max_time = max_time,
                   event = event, timescale = timescale)
+
   
   ## create argument list to be passed to split_data
   dots <- list(...)
   dots$multiple_id <- TRUE # possible in case of multi-state models with back transitions
-  
+
   # if id allready in the data set, remove id variable from dots but keep
   # id variable for later rearrangment
   if (!is.null(dots$id)) {
@@ -146,53 +269,56 @@ split_data_multistate <- function(
     dots$id <- id_var
   }
   
-  split_df_list <- map2(
-    .x = data_list,
-    .y = ifelse(is.list(cuts), cuts, list(cuts)),
-    .f = ~ {
-      dots$data    <- .x
-      dots$formula <- formula
-      dots$cut     <- .y
-      split_df     <- do.call(split_data, dots)
-    }
-  )
+  return(dots)
   
-  split_df <- bind_rows(split_df_list)
-  split_df <- split_df %>%
-    arrange(.data[[transition]], .data[[dots$id]], .data[["tstart"]])
-  
-  # remove all obs beyond last observed event time
-  if (is.null(max_time)) {
-    max_time <- max(split_df[["tend"]][split_df[["ped_status"]] == 1])
-    split_df <- split_df %>% filter(.data[["tend"]] <= max_time)
-  }
-  
-  # if (timescale == "calendar") {
-  #   split_check <- split_df %>%
-  #     group_by(.data[[dots$id]]) %>%
-  #     summarize(dups = sum(duplicated(.data[["tstart"]])))
-  
-  #   if (any(split_check[["dups"]]) != 0) {
-  #     stop("Something went wrong during data transformation. \n Please post an issue at 'https://github.com/adibender/pammtools/issues' with your code and data")
+  # 
+  # split_df_list <- map2(
+  #   .x = data_list,
+  #   .y = ifelse(is.list(cuts), cuts, list(cuts)),
+  #   .f = ~ {
+  #     dots$data    <- .x
+  #     dots$formula <- formula
+  #     dots$cut     <- .y
+  #     split_df     <- do.call(split_data, dots)
   #   }
+  # )
+  # 
+  # split_df <- bind_rows(split_df_list)
+  # split_df <- split_df %>%
+  #   arrange(.data[[transition]], .data[[dots$id]], .data[["tstart"]])
+  # 
+  # # remove all obs beyond last observed event time
+  # if (is.null(max_time)) {
+  #   max_time <- max(split_df[["tend"]][split_df[["ped_status"]] == 1])
+  #   split_df <- split_df %>% filter(.data[["tend"]] <= max_time)
   # }
-  
-  ## set class and and attributes
-  class(split_df) <- c("ped", class(split_df))
-  attr(split_df, "breaks") <- cuts
-  attr(split_df, "id_var") <- dots_in$id <- id_var
-  attr(split_df, "intvars") <- c(id_var, "tstart", "tend", "interval", "offset",
-                                 "ped_status")
-  dots_in$transition           <- transition
-  dots_in$timescale            <- timescale
-  dots_in$cut                  <- sort(unique(cuts))
-  dots_in$max_time             <- max_time
-  dots_in$event                <- event
-  dots_in$min_events           <- min_events
-  attr(split_df, "trafo_args") <- dots_in
-  class(split_df)              <- unique(class(split_df))
-  
-  split_df
+  # 
+  # # if (timescale == "calendar") {
+  # #   split_check <- split_df %>%
+  # #     group_by(.data[[dots$id]]) %>%
+  # #     summarize(dups = sum(duplicated(.data[["tstart"]])))
+  # 
+  # #   if (any(split_check[["dups"]]) != 0) {
+  # #     stop("Something went wrong during data transformation. \n Please post an issue at 'https://github.com/adibender/pammtools/issues' with your code and data")
+  # #   }
+  # # }
+  # 
+  # ## set class and and attributes
+  # class(split_df) <- c("ped", class(split_df))
+  # attr(split_df, "breaks") <- cuts
+  # attr(split_df, "id_var") <- dots_in$id <- id_var
+  # attr(split_df, "intvars") <- c(id_var, "tstart", "tend", "interval", "offset",
+  #                                "ped_status")
+  # dots_in$transition           <- transition
+  # dots_in$timescale            <- timescale
+  # dots_in$cut                  <- sort(unique(cuts))
+  # dots_in$max_time             <- max_time
+  # dots_in$event                <- event
+  # dots_in$min_events           <- min_events
+  # attr(split_df, "trafo_args") <- dots_in
+  # class(split_df)              <- unique(class(split_df))
+  # 
+  # split_df
   
 }
 
