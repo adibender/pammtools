be0e81482aed51835fbdd86debf3855413ece78b
adibender <bender.at.R@gmail.com>
2025-03-20 14:09:39 +0100

    Update reference

--- R/pammtools.R
+++ R/pammtools.R
@@ -50,7 +50,7 @@
 #'
 #' Bender, Andreas, and Fabian Scheipl. 2018.
 #' “pammtools: Piece-Wise Exponential Additive Mixed Modeling Tools.”
-#' ArXiv:1806.01042 [Stat], June. https://arxiv.org/abs/1806.01042.
+#' ArXiv:1806.01042 Stat, June. https://arxiv.org/abs/1806.01042.
 
 #' Ramjith J, Bender A, Roes KCB, Jonker MA. Recurrent events analysis
 #' with piece-wise exponential additive mixed models. 2022. Statistical
