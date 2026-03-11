009cff730b3c15ea47426f41a042b702460a3126
fabian-s <fabian.scheipl@googlemail.com>
2026-03-11 09:29:41 +0100

    Register fdl smooth constructor with mgcv

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,25 @@
-.onLoad <- function(libname=find.package("pammtools"), pkgname="pammtools") {
+.onLoad <- function(
+  libname = find.package("pammtools"),
+  pkgname = "pammtools"
+) {
+  if (requireNamespace("mgcv", quietly = TRUE)) {
+    registerS3method(
+      "smooth.construct",
+      "fdl.smooth.spec",
+      smooth.construct.fdl.smooth.spec,
+      envir = asNamespace("mgcv")
+    )
+  }
 
   if (getRversion() >= "2.5.1") {
-
     utils::globalVariables(c(".", "id"))
-
   }
   invisible()
-
 }
 
-.onAttach <- function(libname=find.package("pammtools"), pkgname="pammtools") {
-
+.onAttach <- function(
+  libname = find.package("pammtools"),
+  pkgname = "pammtools"
+) {
   invisible()
 }
